6c0dc0a20d5729fb62318a518578f0d920537020
A. Merezhanyi <a.merezhanyi@gmail.com>
2023-01-29 18:31:52 +0100

    addressed lint issues

---
+++ src/main/java/nextmainfocus/tasklist/TasksController.java
@@ -0,0 +1,190 @@
+package nextmainfocus.tasklist;
+
+import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.Nullable;
+import nextmainfocus.tasklist.entity.Task;
+
+// Spring boot workflow:
+// front            back
+// httpRequest > controller > service (magic here) > repo (interface) > entities (db)
+//             < controller < service (magic here) < repo (interface) < entities (db)
+
+@CrossOrigin(origins = "${client.url}")
+@RestController
+@RequestMapping("api/v1/")
+public class TasksController {
+	private static final Logger logger = LoggerFactory.getLogger(TasksController.class);
+
+	private static final String ID = "id";
+	private static final String CODE_OK = "OK";
+	private static final String CODE_NOT_FOUND = "NOT_FOUND";
+	private static final String CODE_SERVER_ERROR = "INTERNAL_SERVER_ERROR";
+	private static final String MESSAGE_FOUND = "Task was found";
+	private static final String MESSAGE_NOT_FOUND = "😱 No tasks found";
+	private static final String MESSAGE_CREATED = "Task was created";
+	private static final String MESSAGE_CREATION_FAILED = "😱 Task creation failed.";
+	private static final String MESSAGE_DELETED = "Task was deleted";
+	private static final String MESSAGE_DELETION_FAILED = "😱 Deleting failed";
+	private static final String MESSAGE_UPDATED = "Task was updated.";
+
+	private static String fillBody(String code, String message, @Nullable List<Task> results) {
+		JSONObject body = new JSONObject();
+		body.put("code", code);
+		body.put("message", message);
+		body.put("results", results);
+		return body.toString();
+	}
+
+	@Autowired
+	TasksService tasksService;
+
+	@GetMapping(value = "tasks/", produces = "application/json")
+	public ResponseEntity<String> findTasks(@RequestParam(required = false) String page,
+			@RequestParam(required = false) String sort, @RequestParam(required = false) String field) {
+		logger.info("Received request to retrieve tasks");
+
+		String body;
+		HttpStatus status;
+		// TODO: catch 500 for tasksService
+		List<Task> tasks = tasksService.findAllTasks(page, sort, field);
+
+		if (!tasks.isEmpty()) {
+			logger.info("{} tasks were found in the database", tasks.size());
+
+			body = fillBody(CODE_OK, MESSAGE_FOUND, tasks);
+			status = HttpStatus.OK;
+		} else {
+			logger.error("No tasks were found in the database");
+
+			body = fillBody(CODE_NOT_FOUND, MESSAGE_NOT_FOUND, null);
+			status = HttpStatus.NOT_FOUND;
+		}
+		return new ResponseEntity<>(body, status);
+	}
+
+	@GetMapping(value = "tasks/{id}", produces = "application/json")
+	public ResponseEntity<String> findTask(@PathVariable(ID) UUID id) {
+		// TODO: validate UUID
+		logger.info("Received request to retrieve task with ID: {}", id);
+
+		String body;
+		HttpStatus status;
+		// TODO: catch 500 for tasksService
+		Task task = tasksService.findById(id);
+
+		if (task != null) {
+			logger.info("A task found with ID: {}", id);
+
+			body = fillBody(CODE_OK, MESSAGE_FOUND, Arrays.asList(task));
+			status = HttpStatus.OK;
+		} else {
+			logger.error("A task was not found with ID={}", id);
+
+			body = fillBody(CODE_NOT_FOUND, MESSAGE_NOT_FOUND + ": " + id, null);
+			status = HttpStatus.NOT_FOUND;
+		}
+		return new ResponseEntity<>(body, status);
+	}
+
+	@PostMapping(value = "tasks/", produces = "application/json")
+	public ResponseEntity<String> createTask(@RequestBody Task task) {
+		logger.info("Received request to create a new task:");
+		logger.info("description: {}", task.getDescription());
+		logger.info("date/time: {}", task.getDateTime());
+		logger.info("done: {}", task.isDone());
+
+		HttpStatus status;
+		String body;
+
+		try {
+			Task newTask = tasksService.createTask(task);
+			logger.info("Task was created with description: {}", task.getDescription());
+
+			body = fillBody(CODE_OK, MESSAGE_CREATED, Arrays.asList(newTask));
+			status = HttpStatus.OK;
+		} catch (Exception exception) {
+			logger.info("Task was not created with description: {}", task.getDescription());
+			logger.error("due to: {}", exception.getMessage());
+
+			body = fillBody(CODE_SERVER_ERROR, MESSAGE_CREATION_FAILED, null);
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<>(body, status);
+	}
+
+	@PutMapping(value = "tasks/{id}", produces = "application/json")
+	public ResponseEntity<String> updateTask(@PathVariable(ID) UUID id, @RequestBody Task task) {
+		// TODO: validate UUID
+		logger.info("Received request to update task with ID={}", id);
+
+		String body;
+		HttpStatus status;
+		// TODO: catch 500 for tasksService
+		Task updatedTask = tasksService.updateTask(id, task);
+
+		if (updatedTask != null) {
+			logger.info("A task updated with ID={}", id);
+
+			body = fillBody(CODE_OK, MESSAGE_UPDATED, Arrays.asList(updatedTask));
+			status = HttpStatus.OK;
+		} else {
+			logger.error("A task was not updated with ID={}", id);
+
+			body = fillBody(CODE_NOT_FOUND, MESSAGE_NOT_FOUND + ": " + id, null);
+			status = HttpStatus.NOT_FOUND;
+		}
+		return new ResponseEntity<>(body, status);
+	}
+
+	@DeleteMapping(value = "tasks/{id}", produces = "application/json")
+	public ResponseEntity<String> deleteTask(@PathVariable(ID) UUID id) {
+		// TODO: validate UUID
+		logger.info("Received request to delete task with ID={}", id);
+
+		String body;
+		HttpStatus status;
+
+		try {
+			tasksService.deleteTask(id);
+			logger.info("A task deleted with ID={}", id);
+
+			body = fillBody(CODE_OK, MessageFormat.format("{0}: {1}", MESSAGE_DELETED, id), null);
+			status = HttpStatus.OK;
+		} catch (IllegalArgumentException | EmptyResultDataAccessException exception) {
+			logger.error("A task was not deleted with ID={} due to:", id);
+			logger.error(exception.getMessage());
+
+			body = fillBody(CODE_NOT_FOUND, MessageFormat.format("{}: {}", MESSAGE_NOT_FOUND, id), null);
+			status = HttpStatus.NOT_FOUND;
+		} catch (Exception exception) {
+			logger.error("A task was not deleted with ID={} due to:", id);
+			logger.error(exception.getMessage());
+
+			body = fillBody(CODE_SERVER_ERROR, MessageFormat.format("{}: {}", MESSAGE_DELETION_FAILED, id), null);
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<>(body, status);
+	}
+}
--- src/main/resources/database/seed.sql
+++ src/main/resources/database/seed.sql
@@ -1,3 +1,6 @@
 INSERT INTO "task" ("id", "date_time", "description", "is_done") VALUES
-(1,	'2023-01-28 08:40:54.597621',	'Drink morning coffee',	't'),
-(2,	'2023-01-29 09:00:20.57942',	'Learn AWS',	'f');
+('27823a29-ff44-42e0-a51f-48d49e8a256c',	'2023-01-28 08:40:54.597621',	'Drink morning coffee',	't'),
+('289cac97-2a4d-4af0-a634-62edb19850cd',	'2023-01-29 09:00:20.57942',	'Learn AWS',	'f');
+
+INSERT INTO "account" ("id", "password", "role", "username") VALUES
+('f045ec41-1d59-4886-b264-cd4332171412',	'5f4dcc3b5aa765d61d8327deb882cf99',	'user',	'guest');
