8280990f6e5f0786687a6af95c0a53901e93b4cf
A. Merezhanyi <a.merezhanyi@gmail.com>
2023-01-31 01:34:37 +0100

    Task component refactoring (#12)
    * addressed lint issues
    
    * added UUID validation
    
    * reorder
    
    * updated YML description

--- src/main/java/nextmainfocus/tasks_list/repository/TasksRepository.java
+++
@@ -1,8 +0,0 @@
-package nextmainfocus.tasks_list.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import nextmainfocus.tasks_list.entity.Task;
-
-public interface TasksRepository extends JpaRepository<Task, Long> {
-}
--- src/main/resources/database/schema.sql
+++ src/main/resources/database/schema.sql
@@ -1,9 +1,6 @@
 DROP TABLE IF EXISTS "account";
-DROP SEQUENCE IF EXISTS account_id_seq;
-CREATE SEQUENCE account_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1;
-
 CREATE TABLE "public"."account" (
-    "id" bigint DEFAULT nextval('account_id_seq') NOT NULL,
+    "id" uuid NOT NULL,
     "password" character varying(255),
     "role" character varying(255),
     "username" character varying(255),
@@ -12,11 +9,8 @@ CREATE TABLE "public"."account" (
 
 
 DROP TABLE IF EXISTS "task";
-DROP SEQUENCE IF EXISTS task_id_seq;
-CREATE SEQUENCE task_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1;
-
 CREATE TABLE "public"."task" (
-    "id" bigint DEFAULT nextval('task_id_seq') NOT NULL,
+    "id" uuid NOT NULL,
     "date_time" timestamp(6),
     "description" character varying(255),
     "is_done" boolean NOT NULL,
