2ab950e048c58aadef2d426453274f708975eef3
A. Merezhanyi <a.merezhanyi@gmail.com>
2023-02-11 23:13:16 +0100

    clean up

--- src/main/java/nextmainfocus/jwt/JwtAuthenticationFilter.java
+++ src/main/java/nextmainfocus/jwt/JwtAuthenticationFilter.java
@@ -26,16 +26,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		// final String authHeader = request.getHeader(JwtService.AUTHORIZATION);
-		// final String jwt;
-		// final String username;
-
-		// if (authHeader == null || !authHeader.startsWith(JwtService.BEARER)) {
-		// 	filterChain.doFilter(request, response);
-		// 	return;
-		// }
-		// jwt = authHeader.substring(7);
-		// username = jwtService.extractUsername(jwt);
 		var tokenMap = jwtService.extractTokenFromHeader(request.getHeader(jwtService.authorizationHeader));
 		String jwt = tokenMap.get("token");
 		String username = tokenMap.get("username");
